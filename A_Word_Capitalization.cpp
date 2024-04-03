7104d95ef6a4c05257075c930f78ebd3b5fd3aaf
kareemessam09 <kareemessam.me@gmail.com>
2024-04-03 14:41:01 +0200

    solved

---
+++ A_Word_Capitalization.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define max 100000
+#define all(x) (x).begin(), (x).end()
+#define KemOn09() ios_base::sync_with_stdio(false), cin.tie(0), cout.tie(0)
+#define isEven(n) (n % 2 ==0)
+
+
+int main() {
+KemOn09();
+
+string s;
+cin >> s;
+s.at(0) = toupper(s.at(0));
+cout << s << endl;
+
+
+
+return 0;
+}
