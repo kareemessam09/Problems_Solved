c25ef93eb48c5025a18733280a811641fce2d348
kareemessam09 <kareemessam.me@gmail.com>
2024-05-12 21:48:56 +0300

    solved

---
+++ K_Prime_Fibonacci.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define max 100000
+#define all(x) (x).begin(), (x).end()
+#define KemOn09() ios_base::sync_with_stdio(false), cin.tie(0), cout.tie(0)
+#define isEven(n) (n % 2 ==0)
+
+
+
+int main() {
+KemOn09();
+int num;
+    bool prime[51] = { 0,0,0,0,1,1,1,0,1,0,0,0,1,0,1,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,1,0,0 };
+    int t;
+	cin>>t;
+	while(t--){
+		cin>>num;
+		if(prime[num] == 1)
+		  cout<<"prime\n";
+		  else 
+		  cout<<"not prime\n";
+	}  
+
+return 0;
+}
