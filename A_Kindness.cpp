e81db0f5195b503ab156740146b8e9f599d47f0f
kareemessam09 <kareemessam.me@gmail.com>
2024-07-17 23:57:17 +0300

    solved

---
+++ A_Kindness.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+
+
+using namespace std;
+#define ll long long
+#define mx 100000
+#define all(x) (x).begin(), (x).end()
+#define KemOn09() ios_base::sync_with_stdio(false), cin.tie(0), cout.tie(0);
+#define isEven(n) (n % 2 ==0)
+
+int arr[100001];
+vector<int> v;
+
+int main() {
+KemOn09();
+
+string s;
+cin>>s;
+cout<<s.length();
+
+
+
+
+
+return 0;
+}
