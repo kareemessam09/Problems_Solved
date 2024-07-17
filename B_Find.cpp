e9c705627bd1e77b04d14bfbe75ccd9b1953409d
kareemessam09 <kareemessam.me@gmail.com>
2024-07-18 00:05:09 +0300

    solved

---
+++ B_Find.cpp
@@ -0,0 +1,47 @@
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+ 
+using namespace __gnu_pbds;
+using namespace std;
+#define ll long long
+#define mx 100000
+#define all(x) (x).begin(), (x).end()
+#define KemOn09() ios_base::sync_with_stdio(false), cin.tie(0), cout.tie(0);
+#define isEven(n) (n % 2 ==0)
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> orderedSet;
+void Erase(orderedSet &s, int val){
+    int rank = s.order_of_key(val);
+    auto it = s.find_by_order(rank);
+    s.erase(it);
+}
+int arr[100001];
+vector<int> v;
+
+int main() {
+KemOn09();
+
+int n;
+cin >> n;
+int in;
+cin>>in;
+int c;
+int k;
+while (n--)
+{
+    cin>>k;
+    if (k==in)
+    {
+        cout<<c;
+        return 0;
+    }
+    c++;
+    
+}
+
+cout<<"Not Found";
+
+
+
+return 0;
+}
