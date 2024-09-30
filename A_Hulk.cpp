f897741a183b27561d6a672b1c8d427fa5c5e1ec
kareemessam09 <kareemessam.me@gmail.com>
2024-09-30 23:38:57 +0300

    not solved

---
+++ A_Hulk.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+ 
+using namespace std;
+#define ll long long
+#define mx 100000
+#define all(x) (x).begin(), (x).end()
+#define isEven(n) (n % 2 ==0)
+
+
+void solve(){
+
+  
+    
+
+}
+
+int main() {
+ios_base::sync_with_stdio(false), cin.tie(0), cout.tie(0);
+
+//solve();
+
+  int n;
+    cin>>n;
+    if ( n == 1)
+    {
+        cout<<"I hate it";
+    }
+    
+    if (n == 2)
+    {
+        cout<<"I hate that I love it";
+    }
+    if (n == 3)
+    {
+        cout<<"I hate that I love that I hate it";
+    }
+
+
+return 0;
+}
