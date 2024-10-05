83c7a12ea86e8388e4e9c7b56c25dc5239f16250
kareemessam09 <kareemessam.me@gmail.com>
2024-10-05 21:26:07 +0300

    solved

--- learn.cpp
+++ learn.cpp
@@ -12,8 +12,31 @@ int main() {
 ios_base::sync_with_stdio(false);
 cin.tie(nullptr), cout.tie(nullptr);
 
-int n;
-cin >> n;
+queue<int>qu; //FIFO
+stack<int>st; //LIFO
+deque<int>deq; //Double ended queue
+priority_queue<int>pq; //Max heap by default
+
+unordered_set<int>uset; //Unsorted set
+unordered_map<int, int>umap; //Unsorted map
+
+set<int>se; //Sorted set
+map<int, int>mp; //Sorted map
+
+vector<int>vec; //Dynamic array
+array<int, 10>arr; //Fixed size array
+
+list<int>li; //Double linked list
+
+multiset<int>mset; //Sorted multiset <allows dublicates>
+multimap<int, int>mmp; //Sorted multimap <multi values for same key>
+
+bitset<10>bt; //manage flags or on/off states
+
+pair<int, int>pr; //pair of two values
+
+tuple<int, int, int>tp; //tuple of three values
+
 
 
 
