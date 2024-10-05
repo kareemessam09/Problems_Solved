1be3e6c1ea692293fa7640c7d651ec4c13787972
kareemessam09 <kareemessam.me@gmail.com>
2024-10-05 15:40:14 +0300

    edit some stuff

---
+++ learn.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+ 
+using namespace std;
+#define ll long long
+#define mx 100000
+#define all(x) (x).begin(), (x).end()
+#define isEven(n) (n % 2 ==0)
+
+int main() {
+
+ios_base::sync_with_stdio(false);
+cin.tie(0);
+cout.tie(0);
+
+int n;
+cin >> n;
+
+string s;
+
+
+
+
+
+
+
+return 0;
+}
