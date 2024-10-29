d33eec00f2cb4492efb468f450e40bab232d698b
kareemessam09 <kareemessam.me@gmail.com>
2024-10-30 00:36:30 +0300

    solved

--- B_Card_Game.cpp
+++ B_Card_Game.cpp
@@ -7,13 +7,32 @@ using ll = long long;
 #define rall(x) (x).rbegin(), (x).rend()
 #define pb push_back
 
+int f(int a, int b) {
+    if (a > b) return 1;
+    if (a == b) return 0;
+    return -1;
+}
+
+
 int main() {
 
 ios_base::sync_with_stdio(false);
 cin.tie(nullptr), cout.tie(nullptr);
+int t;
+cin>>t;
+while (t--) {
+        int a, b, c, d;
+        cin >> a >> b >> c >> d;
 
-int n;
-cin >> n;
+        int ans = 0;
+
+        if (f(a, c) + f(b, d) > 0) ans += 1;
+        if (f(a, d) + f(b, c) > 0) ans += 1;
+        if (f(b, c) + f(a, d) > 0) ans += 1;
+        if (f(b, d) + f(a, c) > 0) ans += 1;
+
+        cout << ans << nl;
+    }
 
 
 
