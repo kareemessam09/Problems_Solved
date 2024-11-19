ba82a3548b81aad975e778a6d1b3662c9f48446e
kareemessam09 <kareemessam.me@gmail.com>
2024-11-19 18:41:08 +0200

    add K. Sorting pairs problem solution

---
+++ K_Sorting_pairs.cpp
@@ -0,0 +1,54 @@
+#include<bits/stdc++.h>
+ 
+using namespace std;
+using ll = long long;
+#define nl "\n"
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define pb push_back
+
+struct ss{
+    pair<string,int> p;
+};
+bool compare(const ss &g1, const ss &g2) {
+
+    if (g1.p.second == g2.p.second)
+    {
+        return g1.p.first < g2.p.first;
+    }
+    
+    
+    return g1.p.second > g2.p.second;
+}
+
+int main() {
+
+ios_base::sync_with_stdio(false);
+cin.tie(nullptr), cout.tie(nullptr);
+
+int n;
+cin >> n;
+
+vector<ss> v(7);
+
+
+for (size_t i = 0; i < n; i++)
+{
+    cin>>v[i].p.first >> v[i].p.second;
+}
+
+sort(all(v) , compare);
+
+for (size_t i = 0; i < n; i++)
+{
+    cout<<v[i].p.first <<" "<< v[i].p.second<<nl;
+}
+
+
+
+
+
+
+
+return 0;
+}
