792698ebee8fd6997360056920d7deefd8b5297d
kareemessam09 <kareemessam.me@gmail.com>
2025-03-02 19:05:14 +0200

    Add solution for "A. Even Odds" problem and update "A. Chat room" test cases

---
+++ A_Even_Odds.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+ 
+using namespace std;
+using ll = long long;
+#define nl "\n"
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define pb push_back
+
+int main() {
+
+ios_base::sync_with_stdio(false);
+cin.tie(nullptr), cout.tie(nullptr);
+
+long long n, k;
+    cin >> n >> k;
+    if (k <= (n + 1) / 2)
+    {
+        cout << k * 2 - 1 << endl;
+    }
+    else
+    {
+        cout << (k - (n + 1) / 2) * 2 << endl;
+    }
+
+
+
+
+return 0;
+}
